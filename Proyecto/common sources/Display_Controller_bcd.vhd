838e30b21f73f729aa5088dc59bb0a6ef30b33e5
jjaguillon <jjaguillon@uc.cl>
2019-10-08 10:14:29 -0300

    :zap: :zap: added common files Proyecto

--- Proyecto/common sources/Display_Controller_bcd.vhd
+++ Proyecto/common sources/Display_Controller_bcd.vhd
@@ -6,24 +6,22 @@ use IEEE.std_logic_unsigned.all;
 
 entity Display_Controller is
     Port (
-        dis_a       : in   std_logic_vector (3 downto 0);
-        dis_b       : in   std_logic_vector (3 downto 0);
-        dis_c       : in   std_logic_vector (3 downto 0);
-        dis_d       : in   std_logic_vector (3 downto 0);
-        clk         : in   std_logic;
-        seg         : out  std_logic_vector (7 downto 0);
-        an          : out  std_logic_vector (3 downto 0)
+        dis_a       : in   STD_LOGIC_VECTOR (3 downto 0);
+        dis_b       : in   STD_LOGIC_VECTOR (3 downto 0);
+        dis_c       : in   STD_LOGIC_VECTOR (3 downto 0);
+        dis_d       : in   STD_LOGIC_VECTOR (3 downto 0);
+        clk         : in   STD_LOGIC;
+        seg         : out  STD_LOGIC_VECTOR (7 downto 0);
+        an          : out  STD_LOGIC_VECTOR (3 downto 0)
           );
 end Display_Controller;
 
 architecture Behavioral of Display_Controller is    
 
-constant clock_divide_max : std_logic_vector(16 downto 0) := "11110000100000000";
-
-signal clock_divide_counter: std_logic_vector(16 downto 0) := (others => '0');
-
-signal display : std_logic_vector(1 downto 0) := (others => '0');
-signal number : std_logic_vector(3 downto 0) := (others => '0');
+constant clock_divide_max   : STD_LOGIC_VECTOR (16 downto 0) := "11110000100000000";
+signal clock_divide_counter : STD_LOGIC_VECTOR (16 downto 0) := (others => '0');
+signal display              : STD_LOGIC_VECTOR (1 downto 0) := (others => '0');
+signal number               : STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
 
 begin
 
@@ -36,6 +34,8 @@ clock_divide : process (clk)
             else
                 clock_divide_counter <= clock_divide_counter + 1;
             end if;
+        else
+                -- necesario para poder simular
         end if;
     end process;
 
@@ -43,13 +43,15 @@ with display select
     an <=       "0111" when "00",
                 "1011" when "01",
                 "1101" when "10",
-                "1110" when "11";
+                "1110" when "11",
+                "0000" when others;
           
 with display select
     number <=   dis_a when "00",
                 dis_b when "01",
                 dis_c when "10",
-                dis_d when "11";
+                dis_d when "11",
+                "0000" when others;
    
 with number select
     seg <=      "11000000" when "0000",
@@ -62,11 +64,6 @@ with number select
                 "11111000" when "0111",
                 "10000000" when "1000",
                 "10010000" when "1001",
-                "10001000" when "1010",
-                "10000011" when "1011",
-                "11000110" when "1100",
-                "10100001" when "1101",
-                "10000110" when "1110",
-                "10001110" when "1111";
+                "11111111" when others;
             
 end Behavioral;
